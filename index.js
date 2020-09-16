d2a5fe1d45e05ff5f7f3d95404f44478cdc05eae
Shirlen Detablan <slen.detablan@gmail.com>
2020-09-16 19:21:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 function showMessage(msg) {
-  document.getElementById
+  document.getElementById("")
 }
 /* Write your implementation of displayMessage() */

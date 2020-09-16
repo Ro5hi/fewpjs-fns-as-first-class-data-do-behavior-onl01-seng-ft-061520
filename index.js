97b7702511129b7b1d0286b0bd85c0c91204e3d3
Shirlen Detablan <slen.detablan@gmail.com>
2020-09-16 19:28:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function showMessage(msg) {
 /* Write your implementation of displayMessage() */
 function greet(timeStr) {
   const time = parseInt(timeStr, 10);
-  if (hour > 17) return "Good evening"
-  if (hour < 12) return "Good morning"
-  return "Good afternoon"
+  if (hour > 17) return "Good Evening"
+  if (hour < 12) return "Good Morning"
+  return "Good Afternoon"
 }
